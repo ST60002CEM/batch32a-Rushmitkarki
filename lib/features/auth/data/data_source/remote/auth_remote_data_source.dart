08de18e85ccc3d1378766850fd75248673f875ae
Rushmitkarki <220046@softwarica.edu.np>
2024-06-19 08:15:49 +0545

    register completed from api

--- lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
+++ lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
@@ -2,27 +2,27 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:final_assignment/app/constants/api_endpoint.dart';
 import 'package:final_assignment/core/failure/failure.dart';
+import 'package:final_assignment/core/networking/remote/http_service.dart';
 import 'package:final_assignment/features/auth/data/model/auth_api_model.dart';
-
 import 'package:final_assignment/features/auth/domain/entity/auth_entity.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
- 
+
 final authRemoteDataSourceProvider = Provider<AuthRemoteDataSource>(
   (ref) => AuthRemoteDataSource(
     dio: ref.watch(httpServiceProvider),
     authApiModel: ref.watch(authApiModelProvider),
   ),
 );
- 
+
 class AuthRemoteDataSource {
   final Dio dio;
   final AuthApiModel authApiModel;
- 
+
   AuthRemoteDataSource({
     required this.dio,
     required this.authApiModel,
   });
- 
+
   Future<Either<Failure, bool>> registerUser(AuthEntity authEntity) async {
     try {
       Response response = await dio.post(
