b413435bf572d339c9d1cfba65f0967d20827380
Rushmitkarki <220046@softwarica.edu.np>
2024-07-29 21:48:23 +0545

    start appointment

---
+++ lib/features/appointment/domain/entity/appointment_entity.dart
@@ -0,0 +1,15 @@
+class Appointment {
+  final String patientName;
+  final String email;
+  final DateTime appointmentDate;
+  final String phoneNumber;
+  final String appointmentDescription;
+
+  Appointment({
+    required this.patientName,
+    required this.email,
+    required this.appointmentDate,
+    required this.phoneNumber,
+    required this.appointmentDescription,
+  });
+}
