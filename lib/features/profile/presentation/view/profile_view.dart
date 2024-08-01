30a828c5d25fa588d4d4abef6699626c29157a56
Rushmitkarki <220046@softwarica.edu.np>
2024-08-01 12:27:12 +0545

    fetch appointment list

--- lib/features/profile/presentation/view/profile_view.dart
+++ lib/features/profile/presentation/view/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:final_assignment/features/myappointment/presentation/view/appointment_list_view.dart';
 import 'package:final_assignment/features/profile/presentation/viewmodel/profile_view_model.dart';
 import 'package:final_assignment/features/profile/presentation/widgets/profile_menu.dart';
 import 'package:final_assignment/features/profile/presentation/widgets/profile_pic.dart';
@@ -65,7 +66,13 @@ class _ProfileViewState extends ConsumerState<ProfileView> {
               text: "My Appointment List",
               icon: "assets/icons/appointment.svg",
               press: () {
-                // Navigate to My Appointment List screen
+                if (mounted) {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const AppointmentListView(),
+                    ),
+                  );
+                }
               },
             ),
             ProfileMenu(
